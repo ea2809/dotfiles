067eeeb23d04b3d87f192d632a67101ed4ba3105
Enrique García Galán <ea2809@gmail.com>
2017-10-20 16:08:14 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir ~/.vim/spell
+ln -s ~/dotfiles/vim/es.utf-8.spl ~/.vim/spell/es.utf-8.spl
+ln -s ~/dotfiles/vim/es.utf-8.sug ~/.vim/spell/es.utf-8.sug
