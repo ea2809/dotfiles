16c4f1d37b7eeab8c9260cebcd81226bb7784be6
Enrique García Galán <enrique.garciagalan@telefonica.com>
2018-07-10 20:19:55 +0200

    Change symbols and fix some visual bugs

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,1 +1,9 @@
 alias o="open ."
+alias e="exit"
+alias dvimrc='nvim ~/dotfiles/vim/vimrc'
+alias dzshrc='nvim ~/dotfiles/zsh/zshrc'
+alias dtmux='nvim ~/dotfiles/tmux/tmux.conf'
+alias daliases='nvim ~/dotfiles/zsh/alias.zsh'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
