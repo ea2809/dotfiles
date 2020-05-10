815b851102b2498c0e9c3549b3015cbbfa8a9c19
Enrique García Galán <enrique.garciagalan@telefonica.com>
2020-05-10 14:08:34 +0200

    Fix some problems with the new kitty version

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install plug
+# Install vim-plug
 if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
