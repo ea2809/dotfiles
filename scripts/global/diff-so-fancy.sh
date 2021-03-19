2773297325982b8458d604e3d446f85c7acaaacf
Enrique García Galán <enrique.garciagalan@telefonica.com>
2021-03-19 17:00:17 +0100

    Add diff-so-fancy

---
+++ scripts/global/diff-so-fancy.sh
@@ -0,0 +1,6 @@
+# thanks to https://gist.github.com/gmolveau/8fcdb5e953bd6c9dad18ecd39b9718a4
+path=/usr/local/bin
+cd "$path"
+git clone https://github.com/so-fancy/diff-so-fancy diffsofancy
+chmod +x diffsofancy/diff-so-fancy
+ln -s "$path/diffsofancy/diff-so-fancy" "$path/diff-so-fancy"
--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
 sudo apt-get install -y build-essential curl file git
-sudo apt-get install -y diff-so-fancy zsh tmux
+sudo apt-get install -y zsh tmux bat ripgrep fzy
+
+sudo snap install lsd
