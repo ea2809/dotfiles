0a9234ed5368093a910c7d47a96b8988ac87da43
Enrique García Galán <enrique.garciagalan@telefonica.com>
2021-03-19 17:14:30 +0100

    Fix diff-so-fancy path

--- scripts/global/diff-so-fancy.sh
+++ scripts/global/diff-so-fancy.sh
@@ -1,5 +1,5 @@
 # thanks to https://gist.github.com/gmolveau/8fcdb5e953bd6c9dad18ecd39b9718a4
-path=/usr/local/bin
+path=/usr/bin
 cd "$path"
 git clone https://github.com/so-fancy/diff-so-fancy diffsofancy
 chmod +x diffsofancy/diff-so-fancy
